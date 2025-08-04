8d0ffa70c6579a15c57eaa211fdecfd4ea5576d9
Varshith <sriramoju.varshith@outlook.com>
2025-08-04 20:25:30 +0530

    commit

---
+++ TMKS requirements.bat
@@ -0,0 +1,9 @@
+pip install --user Django
+pip install --user pandas
+pip install --user mysqlclient
+pip install --user xlwt
+pip install --user scikit-learn
+pip install --user openpyxl
+
+
+pause
